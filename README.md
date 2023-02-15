734731c817de24e9fe17618a8787ccdf67209558
Timothy Hilliard <thilliard@linkedin.com>
2023-02-15 11:33:26 -0500

    Add tab closer project first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Launcher Tab Closer
+
+Automatically cleans up tabs left behind when opening teams and zoom meetings as well as the sso callback pages left by global protect.
---
+++ background.js
@@ -0,0 +1,26 @@
+const filter = {
+    url: [
+        {hostEquals: 'teams.microsoft.com', pathPrefix: '/dl/launcher/'},
+        {hostEquals: 'linkedin.zoom.us', pathPrefix: '/j/'},
+        {hostPrefix: 'vpn-', hostSuffix: '.corp.linkedin.com', pathEquals: '/SAML20/SP/ACS'},
+    ],
+};
+
+chrome.webNavigation.onCompleted.addListener((tab) => {
+    // Handle a browser navagiation event
+    let newUrl = new URL(tab.url)
+    console.debug(tab)
+    console.debug(newUrl)
+
+    // sleep time expects milliseconds
+    function sleep (time) {
+        return new Promise((resolve) => setTimeout(resolve, time));
+    }
+
+    // We sleep for a second before closing the tab to let any meta content
+    // execute like opening zoom or teams
+    sleep(2000).then(() => {
+        console.log(`Auto closing tab ${tab.url}`)
+        chrome.tabs.remove([tab.tabId])
+    })
+}, filter);
