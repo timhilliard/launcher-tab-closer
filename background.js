53fdeccebf8e32deab9ac04cfd274e549e1b52dd
Timothy Hilliard <thilliard@linkedin.com>
2023-02-16 11:05:55 -0500

    Add manifest v2 under new branch

--- background.js
+++ background.js
@@ -1,26 +1,35 @@
-const filter = {
-    url: [
-        {hostEquals: 'teams.microsoft.com', pathPrefix: '/dl/launcher/'},
-        {hostSuffix: '.zoom.us', pathPrefix: '/j/'},
-        {pathEquals: '/SAML20/SP/ACS'},
-    ],
-};
+let autoCloseDomainFilters = [
+    {hostEquals: 'teams.microsoft.com', pathPrefix: '/dl/launcher/'},
+    {hostEquals: 'linkedin.zoom.us', pathPrefix: '/j/'},
+    {pathEquals: '/SAML20/SP/ACS'},
+]
 
-chrome.webNavigation.onCompleted.addListener((tab) => {
+// sleep time expects milliseconds
+function sleep (time) {
+    return new Promise((resolve) => setTimeout(resolve, time));
+}
+
+function onError(e) {
+    console.error("e", e)
+}
+
+function autoCloseHandler(navEvent) {
     // Handle a browser navagiation event
-    let newUrl = new URL(tab.url)
-    console.debug(tab)
+    let newUrl = new URL(navEvent.url)
+    console.debug(navEvent)
     console.debug(newUrl)
-
-    // sleep time expects milliseconds
-    function sleep (time) {
-        return new Promise((resolve) => setTimeout(resolve, time));
-    }
 
     // We sleep for a second before closing the tab to let any meta content
     // execute like opening zoom or teams
     sleep(2000).then(() => {
-        console.log(`Auto closing tab ${tab.url}`)
-        chrome.tabs.remove([tab.tabId])
+        console.log(`Auto closing tab ${navEvent.url}`)
+        let removing = browser.tabs.remove([navEvent.tabId])
+        removing.then(undefined, onError)
     })
-}, filter);
+}
+
+function main() {
+    browser.webNavigation.onCompleted.addListener(autoCloseHandler, {url: autoCloseDomainFilters})
+}
+
+main()
