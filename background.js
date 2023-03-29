7859cc9b1a7bc3d6f674f2bcc00a9c3b884e79fc
Timothy Hilliard <thilliard@linkedin.com>
2023-03-28 21:23:18 -0400

    Add functionality to for domain filtering with the ability to set config using enterprise policy

--- background.js
+++ background.js
@@ -1,72 +1,151 @@
 const filter = {
     url: [
-        {hostEquals: 'teams.microsoft.com', pathPrefix: '/dl/launcher/'},
-        {hostSuffix: '.zoom.us', pathPrefix: '/j/'},
-        {pathEquals: '/SAML20/SP/ACS'},
+        { hostEquals: 'teams.microsoft.com', pathPrefix: '/dl/launcher/' },
+        { hostSuffix: '.zoom.us', pathPrefix: '/j/' },
+        { pathEquals: '/SAML20/SP/ACS' },
     ],
 };
 
+var configs = {
+    teams: {
+        enabled: true,
+        delay: 2
+    },
+    zoom: {
+        enabled: true,
+        delay: 2,
+        user_domains: [],
+        managed_domains: [],
+        domains: new Set()
+    },
+    globalprotect: {
+        enabled: true,
+        delay: 2,
+        user_domains: [],
+        managed_domains: [],
+        domains: new Set()
+    }
+};
+
 // sleep time expects seconds
-function sleep (time) {
+function sleep(time) {
     // Convert sleep time in seconds to milliseconds for use in setTimeout
     return new Promise((resolve) => setTimeout(resolve, time * 1000));
 }
+
+function validate_domain(domain, rules) {
+    for (const rule of rules) {
+        if (rule.startsWith("*")) {
+            if (domain.endsWith(rule.substring(1))) {
+                console.debug("Domain " + domain + " matches rule " + rule);
+                return true;
+            }
+        } else {
+            if (domain == rule) {
+                console.debug("Domain " + domain + " matches rule " + rule);
+                return true;
+            }
+        }
+    }
+    console.debug("Domain " + domain + " does not match any of the rules: " + rules);
+    return false;
+};
+
+function calculate_domains() {
+    configs.zoom.domains = calculate_domain(configs.zoom.user_domains, configs.zoom.managed_domains);
+    configs.globalprotect.domains = calculate_domain(configs.globalprotect.user_domains, configs.globalprotect.managed_domains);
+}
+
+function calculate_domain(user_domains, managed_domains) {
+    const _union = new Set(user_domains);
+    for (const domain of managed_domains) {
+        _union.add(domain);
+    }
+    return _union;
+}
+
+function reload_config() {
+    chrome.storage.local.get({
+        teams: true,
+        teamsdelay: 2,
+        zoom: true,
+        zoomdelay: 2,
+        zoomdomains: [],
+        globalprotect: true,
+        globalprotectdelay: 2,
+        globalprotectdomains: []
+    }, function (items) {
+        configs.teams.enabled = items.teams
+        configs.teams.delay = items.teamsdelay
+        configs.zoom.enabled = items.zoom
+        configs.zoom.delay = items.zoomdelay
+        configs.zoom.user_domains = items.zoomdomains
+        configs.globalprotect.enabled = items.globalprotect
+        configs.globalprotect.delay = items.globalprotectdelay
+        configs.globalprotect.user_domains = items.globalprotectdomains
+        calculate_domains();
+    });
+
+    chrome.storage.managed.get({
+        zoomdomains: [],
+        globalprotectdomains: []
+    }, function (items) {
+        configs.zoom.managed_domains = items.zoomdomains
+        configs.globalprotect.managed_domains = items.globalprotectdomains
+        calculate_domains();
+    });
+    console.debug("Reloaded config");
+    console.debug(configs);
+}
+
+chrome.runtime.onInstalled.addListener(function () {
+    // Initialize configs
+    reload_config();
+});
+
+// Start observing policy changes.
+chrome.storage.onChanged.addListener(function () {
+    reload_config();
+});
 
 chrome.webNavigation.onCompleted.addListener((tab) => {
     if (tab.frameId === 0) {
         // Handle a browser navigation event
         console.debug(tab)
+        console.debug(configs)
 
         url = new URL(tab.url)
 
-        if (url.hostname == "teams.microsoft.com") {
-            chrome.storage.local.get({
-                teams: true,
-                teamsdelay: 2
-            }, function(items) {
-                console.debug(items)
-                if (items.teams) {
-                    console.debug(`Auto closing teams tab ${tab.url} in ${items.teamsdelay} seconds`)
-                    sleep(items.teamsdelay).then(() => {
-                        console.log(`Auto closing teams tab ${tab.url}`)
-                        chrome.tabs.remove([tab.tabId])
-                    })
-                } else {
-                    console.debug(`Not closing teams tab ${tab.url}`)
+        if (configs.teams.enabled && url.hostname == "teams.microsoft.com") {
+            console.debug(`Auto closing teams tab ${tab.url} in ${configs.teams.delay} seconds`)
+            sleep(configs.teams.delay).then(() => {
+                console.log(`Auto closing teams tab ${tab.url}`)
+                chrome.tabs.remove([tab.tabId])
+            })
+        } else if (configs.zoom.enabled && url.hostname.endsWith(".zoom.us")) {
+            if (configs.zoom.domains.length > 0) {
+                if (!validate_domain(url.hostname, configs.zoom.domains)) {
+                    console.debug("Domain does not match config, skipping...")
+                    return;
                 }
-            });
-        } else if (url.hostname.endsWith(".zoom.us")) {
-            chrome.storage.local.get({
-                zoom: true,
-                zoomdelay: 2
-            }, function(items) {
-                console.debug(items)
-                if (items.zoom) {
-                    console.debug(`Auto closing zoom tab ${tab.url} in ${items.zoomdelay} seconds`)
-                    sleep(items.zoomdelay).then(() => {
-                        console.log(`Auto closing zoom tab ${tab.url}`)
-                        chrome.tabs.remove([tab.tabId])
-                    })
-                } else {
-                    console.debug(`Not closing zoom tab ${tab.url}`)
+            }
+            console.debug(`Auto closing zoom tab ${tab.url} in ${configs.zoom.delay} seconds`)
+            sleep(configs.zoom.delay).then(() => {
+                console.log(`Auto closing zoom tab ${tab.url}`)
+                chrome.tabs.remove([tab.tabId])
+            })
+        } else if (configs.globalprotect.enabled && url.pathname == "/SAML20/SP/ACS") {
+            if (configs.globalprotect.domains.length > 0) {
+                if (!validate_domain(url.hostname, configs.globalprotect.domains)) {
+                    console.debug("Domain does not match config, skipping...")
+                    return;
                 }
-            });
-        } else if (url.pathname == "/SAML20/SP/ACS") {
-            chrome.storage.local.get({
-                globalprotect: true,
-                globalprotectdelay: 2
-            }, function(items) {
-                console.debug(items)
-                if (items.globalprotect) {
-                    console.debug(`Auto closing global protect tab ${tab.url} in ${items.globalprotectdelay} seconds`)
-                    sleep(items.globalprotectdelay).then(() => {
-                        console.log(`Auto closing global protect tab ${tab.url}`)
-                        chrome.tabs.remove([tab.tabId])
-                    })
-                } else {
-                    console.debug(`Not closing global protect tab ${tab.url}`)
-                }
-            });
+            }
+            console.debug(`Auto closing global protect tab ${tab.url} in ${configs.globalprotect.delay} seconds`)
+            sleep(configs.globalprotect.delay).then(() => {
+                console.log(`Auto closing global protect tab ${tab.url}`)
+                chrome.tabs.remove([tab.tabId])
+            })
         }
     }
 }, filter);
--- options.js
+++ options.js
@@ -1,48 +1,138 @@
+const url_matcher = /(https?:\/\/)?/;
+
 // Saves options to chrome.storage
 function save_options() {
-    var globalprotect = document.getElementById('globalprotect').checked;
-    var globalprotectdelay = Number(document.getElementById('globalprotect-delay').value);
-    var zoom = document.getElementById('zoom').checked;
-    var zoomdelay = Number(document.getElementById('zoom-delay').value);
-    var teams = document.getElementById('teams').checked;
-    var teamsdelay = Number(document.getElementById('teams-delay').value);
+  var globalprotect = document.getElementById('globalprotect').checked;
+  var globalprotectdelay = Number(document.getElementById('globalprotect-delay').value);
+  var globalprotectdomains = document.getElementById('globalprotect-domains').value.trim();
+  var zoom = document.getElementById('zoom').checked;
+  var zoomdelay = Number(document.getElementById('zoom-delay').value);
+  var zoomdomains = document.getElementById('zoom-domains').value.trim();
+  var teams = document.getElementById('teams').checked;
+  var teamsdelay = Number(document.getElementById('teams-delay').value);
+  var statuscontainer = document.getElementById('status');
+  chrome.storage.local.set({
+    globalprotect: globalprotect,
+    globalprotectdelay: globalprotectdelay,
+    zoom: zoom,
+    zoomdelay: zoomdelay,
+    teams: teams,
+    teamsdelay: teamsdelay
+  }, function () {
+    // Update status to let user know options were saved.
+    var status = document.createElement('div');
+    status.innerText = 'Options saved.';
+    statuscontainer.appendChild(status);
+    setTimeout(function () {
+      status.remove();
+    }, 750);
+  });
+  globalprotecterrors = []
+  if (globalprotectdomains == "") {
+    globalprotectdomains = [];
+  } else {
+    globalprotectdomains = globalprotectdomains.split("\n")
+    globalprotectdomains.forEach(function (domain) {
+      if (!url_matcher.test(domain)) {
+        globalprotecterrors.append("Invalid domain: " + domain);
+      }
+    });
+  }
+  if (globalprotecterrors.length > 0) {
+    // print errors, don't save
+    var status = document.createElement('div');
+    status.innerText = globalprotecterrors.join("<br/>");
+    statuscontainer.appendChild(status);
+    setTimeout(function () {
+      status.remove();
+    }, 750);
+  } else {
+    // save entries
     chrome.storage.local.set({
-      globalprotect: globalprotect,
-      globalprotectdelay: globalprotectdelay,
-      zoom: zoom,
-      zoomdelay: zoomdelay,
-      teams: teams,
-      teamsdelay: teamsdelay
-    }, function() {
+      globalprotectdomains: globalprotectdomains
+    }, function () {
       // Update status to let user know options were saved.
-      var status = document.getElementById('status');
-      status.textContent = 'Options saved.';
-      setTimeout(function() {
-        status.textContent = '';
+      var status = document.createElement('div');
+      status.innerText = 'Global Protect domains saved.';
+      statuscontainer.appendChild(status);
+      setTimeout(function () {
+        status.remove();
       }, 750);
     });
   }
-  
-  // Restores current state from chrome.storage
-  function restore_options() {
-    chrome.storage.local.get({
-      globalprotect: true,
-      globalprotectdelay: 2,
-      zoom: true,
-      zoomdelay: 2,
-      teams: true,
-      teamsdelay: 2
-    }, function(items) {
-      console.debug(items)
-      document.getElementById('globalprotect').checked = items.globalprotect;
-      document.getElementById('globalprotect-delay').value = items.globalprotectdelay;
-      document.getElementById('zoom').checked = items.zoom;
-      document.getElementById('zoom-delay').value = items.zoomdelay;
-      document.getElementById('teams').checked = items.teams;
-      document.getElementById('teams-delay').value = items.teamsdelay;
+
+  zoomerrors = []
+  if (zoomdomains == "") {
+    zoomdomains = [];
+  } else {
+    zoomdomains = zoomdomains.split("\n")
+    zoomdomains.forEach(function (domain) {
+      if (!url_matcher.test(domain)) {
+        zoomerrors.append("Invalid domain: " + domain);
+      }
     });
   }
-  document.addEventListener('DOMContentLoaded', restore_options);
-  document.querySelectorAll("input").forEach(function(checkbox) {
-    checkbox.addEventListener('change', save_options);
+  if (zoomerrors.length > 0) {
+    // print errors, don't save
+    var status = document.createElement('div');
+    status.innerText = zoomerrors.join("<br/>");
+    statuscontainer.appendChild(status);
+    setTimeout(function () {
+      status.remove();
+    }, 750);
+  } else {
+    // save entries
+    chrome.storage.local.set({
+      zoomdomains: zoomdomains
+    }, function () {
+      // Update status to let user know options were saved.
+      var status = document.createElement('div');
+      status.innerText = 'Zoom domains saved.';
+      statuscontainer.appendChild(status);
+      setTimeout(function () {
+        status.remove();
+      }, 750);
+    });
+  }
+}
+
+// Restores current state from chrome.storage
+function restore_options() {
+  chrome.storage.local.get({
+    globalprotect: true,
+    globalprotectdelay: 2,
+    globalprotectdomains: [],
+    zoom: true,
+    zoomdelay: 2,
+    zoomdomains: [],
+    teams: true,
+    teamsdelay: 2
+  }, function (items) {
+    console.debug(items)
+    document.getElementById('globalprotect').checked = items.globalprotect;
+    document.getElementById('globalprotect-delay').value = items.globalprotectdelay;
+    document.getElementById('globalprotect-domains').value = items.globalprotectdomains.join("\n");
+    document.getElementById('zoom').checked = items.zoom;
+    document.getElementById('zoom-delay').value = items.zoomdelay;
+    document.getElementById('zoom-domains').value = items.zoomdomains.join("\n");
+    document.getElementById('teams').checked = items.teams;
+    document.getElementById('teams-delay').value = items.teamsdelay;
   });
+
+  chrome.storage.managed.get({
+    globalprotectdomains: [],
+    zoomdomains: []
+  }, function (items) {
+    console.debug(items);
+    document.getElementById('globalprotect-managed-domains').value = items.globalprotectdomains.join("\n");
+    document.getElementById('zoom-managed-domains').value = items.zoomdomains.join("\n");
+  });
+
+  chrome.storage.managed.get(null, function (items) {
+    console.debug(items);
+  });
+}
+document.addEventListener('DOMContentLoaded', restore_options);
+document.querySelectorAll("input,textarea").forEach(function (checkbox) {
+  checkbox.addEventListener('change', save_options);
+});
