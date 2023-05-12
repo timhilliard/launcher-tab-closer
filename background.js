3c1c5e761310e6c4c40f12bf316d2754c5ff10a0
Timothy Hilliard <thilliard@linkedin.com>
2023-05-12 10:54:32 -0400

    Add proper async waiting for config to load before attempting to execute tab closing

--- background.js
+++ background.js
@@ -15,15 +15,21 @@ const configs = {
     enabled: true,
     delay: 2,
     user_domains: [],
+    user_domains_loaded: false,
     managed_domains: [],
+    managed_domains_loaded: false,
     domains: new Set(),
+    loaded: false,
   },
   globalprotect: {
     enabled: true,
     delay: 2,
     user_domains: [],
+    user_domains_loaded: false,
     managed_domains: [],
+    managed_domains_loaded: false,
     domains: new Set(),
+    loaded: false,
   },
 };
 
@@ -47,17 +53,17 @@ function validateDomain(domain, rules) {
   for (const rule of rules) {
     if (rule.startsWith('*')) {
       if (domain.endsWith(rule.substring(1))) {
-        console.debug('Domain ' + domain + ' matches rule ' + rule);
+        console.debug(`Domain ${domain} matches rule ${rule}`);
         return true;
       }
     } else {
       if (domain == rule) {
-        console.debug('Domain ' + domain + ' matches rule ' + rule);
+        console.debug(`Domain ${domain} matches rule ${rule}`);
         return true;
       }
     }
   }
-  console.debug('Domain ' + domain + ' does not match any of the rules: ' + rules);
+  console.debug(`Domain ${domain} does not match any of the rules: ${Array.from(rules)}`);
   return false;
 }
 
@@ -65,14 +71,29 @@ function validateDomain(domain, rules) {
  * Joins user defined and managed domains
  */
 function calculateDomains() {
-  configs.zoom.domains = calculateDomain(
-      configs.zoom.user_domains,
-      configs.zoom.managed_domains,
-  );
-  configs.globalprotect.domains = calculateDomain(
-      configs.globalprotect.user_domains,
-      configs.globalprotect.managed_domains,
-  );
+  if (!configs.zoom.user_domains_loaded || !configs.zoom.managed_domains_loaded) {
+    console.debug('Zoom user or managed configs not yet loaded');
+    console.debug(configs.zoom);
+  } else {
+    configs.zoom.domains = calculateDomain(
+        configs.zoom.user_domains,
+        configs.zoom.managed_domains,
+    );
+    configs.zoom.loaded = true;
+    console.debug('Zoom configs loaded');
+  }
+
+  if (!configs.globalprotect.user_domains_loaded || !configs.globalprotect.managed_domains_loaded) {
+    console.debug('Global Protect user or managed configs not yet loaded');
+    console.debug(configs.globalprotect);
+  } else {
+    configs.globalprotect.domains = calculateDomain(
+        configs.globalprotect.user_domains,
+        configs.globalprotect.managed_domains,
+    );
+    configs.globalprotect.loaded = true;
+    console.debug('Global Protect configs loaded');
+  }
 }
 
 /**
@@ -90,8 +111,8 @@ function calculateDomain(userDomains, managedDomains) {
 }
 
 /** Regenerate the config */
-function reloadConfig() {
-  chrome.storage.local.get({
+async function reloadConfig() {
+  const localLoaded = chrome.storage.local.get({
     teams: true,
     teamsdelay: 2,
     zoom: true,
@@ -100,26 +121,34 @@ function reloadConfig() {
     globalprotect: true,
     globalprotectdelay: 2,
     globalprotectdomains: [],
-  }, function(items) {
+  }).then((items) => {
     configs.teams.enabled = items.teams;
     configs.teams.delay = items.teamsdelay;
     configs.zoom.enabled = items.zoom;
     configs.zoom.delay = items.zoomdelay;
     configs.zoom.user_domains = items.zoomdomains;
+    configs.zoom.user_domains_loaded = true;
     configs.globalprotect.enabled = items.globalprotect;
     configs.globalprotect.delay = items.globalprotectdelay;
     configs.globalprotect.user_domains = items.globalprotectdomains;
+    configs.globalprotect.user_domains_loaded = true;
     calculateDomains();
+    console.debug('Local config loaded');
   });
 
-  chrome.storage.managed.get({
+  const managedLoaded = chrome.storage.managed.get({
     zoomdomains: [],
     globalprotectdomains: [],
-  }, function(items) {
+  }).then((items) => {
     configs.zoom.managed_domains = items.zoomdomains;
+    configs.zoom.managed_domains_loaded = true;
     configs.globalprotect.managed_domains = items.globalprotectdomains;
+    configs.globalprotect.managed_domains_loaded = true;
     calculateDomains();
+    console.debug('Managed config loaded');
   });
+  await localLoaded;
+  await managedLoaded;
   console.debug('Reloaded config');
   console.debug(configs);
 }
@@ -138,13 +167,23 @@ chrome.storage.onChanged.addListener(function() {
 });
 
 /** Main listener for new tabs to take action on */
-chrome.webNavigation.onCompleted.addListener((tab) => {
+chrome.webNavigation.onCompleted.addListener(async (tab) => {
   if (tab.frameId === 0) {
     // Handle a browser navigation event
     console.debug(tab);
     console.debug(configs);
 
     const url = new URL(tab.url);
+
+    // Make sure configs are loaded before running
+    try {
+      console.debug('Waiting for config to load');
+      await initConfig;
+      console.debug('Config loaded');
+    } catch (e) {
+      // Error loading configs. Skipping execution
+      console.error(`Unable to load configs: ${e}`);
+    }
 
     if (configs.teams.enabled && url.hostname == 'teams.microsoft.com') {
       console.debug(`Auto closing teams tab ${tab.url} in ${configs.teams.delay} seconds`);
@@ -153,32 +192,38 @@ chrome.webNavigation.onCompleted.addListener((tab) => {
         chrome.tabs.remove([tab.tabId]);
       });
     } else if (configs.zoom.enabled && url.hostname.endsWith('.zoom.us')) {
+      console.debug(`Processing zoom url ${tab.url}`);
       if (configs.zoom.domains.size > 0) {
         if (!validateDomain(url.hostname, configs.zoom.domains)) {
           console.debug('Domain does not match config, skipping...');
-          return;
+        } else {
+          console.debug(`Auto closing zoom tab ${tab.url} in ${configs.zoom.delay} seconds`);
+          sleep(configs.zoom.delay).then(() => {
+            console.log(`Auto closing zoom tab ${tab.url}`);
+            chrome.tabs.remove([tab.tabId]);
+          });
         }
-        console.debug(`Auto closing zoom tab ${tab.url} in ${configs.zoom.delay} seconds`);
-        sleep(configs.zoom.delay).then(() => {
-          console.log(`Auto closing zoom tab ${tab.url}`);
-          chrome.tabs.remove([tab.tabId]);
-        });
+      } else {
+        console.debug('No config detected for zoom, skipping...');
       }
     } else if (configs.globalprotect.enabled && url.pathname == '/SAML20/SP/ACS') {
       if (configs.globalprotect.domains.size > 0) {
         if (!validateDomain(url.hostname, configs.globalprotect.domains)) {
           console.debug('Domain does not match config, skipping...');
-          return;
+        } else {
+          console.debug(`Auto closing global protect tab ${tab.url} in ${configs.globalprotect.delay} seconds`);
+          sleep(configs.globalprotect.delay).then(() => {
+            console.log(`Auto closing global protect tab ${tab.url}`);
+            chrome.tabs.remove([tab.tabId]);
+          });
         }
-        console.debug(`Auto closing global protect tab ${tab.url} in ${configs.globalprotect.delay} seconds`);
-        sleep(configs.globalprotect.delay).then(() => {
-          console.log(`Auto closing global protect tab ${tab.url}`);
-          chrome.tabs.remove([tab.tabId]);
-        });
+      } else {
+        console.debug('No config detected for global protect, skipping...');
       }
     }
   }
 }, filter);
 
 // Initialize configs
-reloadConfig();
+console.debug('Initializing config');
+const initConfig = reloadConfig();
